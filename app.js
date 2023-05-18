f275bb7f96c6713c5ab3d3c69e53efdb2435e8d3
DKoroli <dimakoroliuk@gmail.com>
2023-05-18 21:43:39 +0300

    18.05.23last

--- app.js
+++ app.js
@@ -14,15 +14,24 @@ const currency = [
     to: "MDL",
     rates: "24.13",
   },
+  {
+    from: "UAH",
+    to: "MDL",
+    rates: "0.45",
+  },
 ];
-
+let i = 0;
 setInterval(function () {
   let currencyFrom = document.getElementById("from");
   let currencyTo = document.getElementById("to");
   let currencyRates = document.getElementById("rates");
-  currencyFrom.innerHTML = currency[0].from;
-  currencyTo.innerHTML = currency[0].to;
-  currencyRates.innerHTML = currency[0].rates;
+  currencyFrom.innerHTML = currency[i].from;
+  currencyTo.innerHTML = currency[i].to;
+  currencyRates.innerHTML = currency[i].rates;
+  i++;
+  if (i > currency.length - 1) {
+    i = 0;
+  }
 }, 5000);
 
 console.log(currency);
