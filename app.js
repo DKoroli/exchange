6cb9bb8539f081156443f0ff0c4df21a9c57960f
DKoroli <dimakoroliuk@gmail.com>
2023-05-25 20:12:48 +0300

    25.05.23

--- app.js
+++ app.js
@@ -20,18 +20,40 @@ const currency = [
     rates: "0.45",
   },
 ];
+
+const obj = {
+  success: true,
+  query: {
+    from: "UAH",
+    to: "MDL",
+    amount: 2,
+  },
+  info: {
+    timestamp: 1684956963,
+    rate: 0.480945,
+  },
+  date: "2023-05-24",
+  result: 0.96189,
+};
+
+const str = JSON.stringify(obj);
+
+const res = JSON.parse(str);
+
+console.log(res);
+
 let i = 0;
 setInterval(function () {
-  let currencyFrom = document.getElementById("from");
-  let currencyTo = document.getElementById("to");
-  let currencyRates = document.getElementById("rates");
+  const currencyFrom = document.getElementById("from");
+  const currencyTo = document.getElementById("to");
+  const currencyRates = document.getElementById("rates");
+  const currencyOnline = document.getElementById("online");
   currencyFrom.innerHTML = currency[i].from;
   currencyTo.innerHTML = currency[i].to;
   currencyRates.innerHTML = currency[i].rates;
+  currencyOnline.innerHTML = res.info.rate;
   i++;
   if (i > currency.length - 1) {
     i = 0;
   }
 }, 5000);
-
-console.log(currency);
